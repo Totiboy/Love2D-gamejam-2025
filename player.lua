94e588702acdd6049a8d97a345d3959c520aca16
freikondev <104316717+Totiboy@users.noreply.github.com>
2025-03-21 15:00:13 +0200

    the player now can't move off-screen

--- player.lua
+++ player.lua
@@ -71,6 +71,8 @@ function player:move(dt)
 ---this sets up the speed to correspond with the direction of the movement and deltatime
     self.x = self.x + self.speed * d.x * dt
     self.y = self.y + self.speed * d.y * dt
+    self.x = math.max(0, math.min(self.x, love.graphics.getWidth() - self.hurtbox.width))
+    self.y = math.max(0, math.min(self.y, love.graphics.getHeight() - self.hurtbox.height))
     self.hurtbox:update(self.x,self.y)
 end
 
@@ -144,27 +146,28 @@ end
 ----------------------------------------------- During Dash ---------------------------------------------------------
 function player:startDash()
     local d = {x = 0, y = 0}
+    
+        if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
+            d.x = d.x + 1
+        end
+        if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
+            d.x = d.x - 1
+        end
+        if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
+            d.y = d.y - 1
+        end
+        if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
+            d.y = d.y + 1
+        end
 
-    if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
-        d.x = d.x + 1
-    end
-    if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
-        d.x = d.x - 1
-    end
-    if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
-        d.y = d.y - 1
-    end
-    if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
-        d.y = d.y + 1
-    end
-
-    -- Normalize direction
-    local length = math.sqrt(d.x^2 + d.y^2)
-    if length > 0 then
-        self.dir_x, self.dir_y = d.x / length, d.y / length
-        self.is_dashing = true
-        self.dash_timer = self.dash_duration
-    end
+        -- Normalize direction
+        local length = math.sqrt(d.x^2 + d.y^2)
+        if length > 0 then
+            self.dir_x, self.dir_y = d.x / length, d.y / length
+            self.is_dashing = true
+            self.dash_timer = self.dash_duration
+        end
+    
 end
 
 ----------------------------------------------- Item Upgrades ---------------------------------------------------------
